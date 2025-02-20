5e29a54842ee92fb800a687d6ecee9b012d21d72
Dixshant shree shreemal <90293938+Dixshant4@users.noreply.github.com>
2025-02-20 21:32:00 +0530

    Update Map.tsx

--- thela-finder/app/components/Map.tsx
+++ thela-finder/app/components/Map.tsx
@@ -413,7 +413,6 @@ export default function ThelaMap({ thelas, onAddThela, onDeleteThela, currentUse
                   className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-emerald-500 
                   text-black bg-white" // Changed text color to black
                   rows={3}
-                  required
                 />
               </div>
               {/* <div>
